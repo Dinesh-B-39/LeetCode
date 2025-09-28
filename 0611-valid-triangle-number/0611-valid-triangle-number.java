dd1bcdd04373a0bd6b8e7c93a30ec5e827032240
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2025-09-28 11:26:17 +0530

    Time: 1673 ms (5.04%), Space: 43.6 MB (25.29%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -1,29 +1,24 @@
 class Solution {
-    public int bsearch(int target,int[] nums,int low,int high)
+    public int check(int c1,int c2,int c3)
     {
-        while(low<=high)
+        if(c1+c2>c3)
         {
-            int mid=(low+high)/2;
-            if(nums[mid]<target)
-            {
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
-            }
+            return 1;
         }
-        return high;
+        return 0;
     }
     public int triangleNumber(int[] nums) {
-        Arrays.sort(nums);
         int c=0;
+        Arrays.sort(nums);
         for(int i=0;i<nums.length-2;i++)
         {
             for(int j=i+1;j<nums.length-1;j++)
             {
-                c+=bsearch(nums[i]+nums[j],nums,j+1,nums.length-1)-j;
+                for(int k=j+1;k<nums.length;k++)
+                {
+                    c+=check(nums[i],nums[j],nums[k]);
 
+                }
             }
         }
         return c;

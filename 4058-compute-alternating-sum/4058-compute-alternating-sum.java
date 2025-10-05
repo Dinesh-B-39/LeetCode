2be2591d65d05e7039eae0a09eccfdec80152e0a
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2025-10-05 16:15:11 +0530

    Time: 0 ms (100%), Space: 44.4 MB (33.33%) - LeetHub

---
+++ 4058-compute-alternating-sum/4058-compute-alternating-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int c1=0;
+        int c2=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i%2==0)
+            {
+                c1+=nums[i];
+            }
+            else
+            {
+                c2+=nums[i];
+            }
+        }
+       return c1-c2; 
+    }
+}

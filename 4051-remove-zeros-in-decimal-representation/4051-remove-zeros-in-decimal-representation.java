3b2c324af8f8994a527d239fc3f88450430f8bfb
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2025-11-05 20:36:57 +0530

    Time: 2 ms (55.93%), Space: 42.7 MB (6.93%) - LeetHub

---
+++ 4051-remove-zeros-in-decimal-representation/4051-remove-zeros-in-decimal-representation.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long removeZeros(long n) {
+        String s=n+"";
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)!='0')
+            {
+                sb.append(s.charAt(i));
+            }
+        }
+        return Long.parseLong(sb.toString());
+    }
+}

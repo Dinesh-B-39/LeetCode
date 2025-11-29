019b1998001cc77041ed476f33b25d7c676bf390
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2025-11-30 00:05:16 +0530

    Time: 1 ms (92.46%), Space: 47.2 MB (5.24%) - LeetHub

---
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int c=0;
+        for(int i:nums)
+        {
+            c+=i;
+        }
+        return c%k;
+        
+    }
+}

f45bec9f7023b11becf81b0b1d9a7ff007ac0912
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2025-11-30 16:46:34 +0530

    Time: 2 ms (67.39%), Space: 42.8 MB (26.11%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()>t.length())
+        {
+            return false;
+        }
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<t.length())
+        {
+            if(s.charAt(i)==t.charAt(j))
+            {
+                i+=1;
+                j+=1;
+            }
+            else
+            {
+                j+=1;
+            }
+        }
+        if(i==s.length())
+        {
+            return true;
+        }
+        return false;
+    }
+}

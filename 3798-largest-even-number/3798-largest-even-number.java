5ca23c550f6084228f64d1bcb8b1584940b69bf9
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-01-04 18:33:28 +0530

    Time: 0 ms (100%), Space: 44.3 MB (34.22%) - LeetHub

---
+++ 3798-largest-even-number/3798-largest-even-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String largestEven(String s) {
+        int i=s.length()-1;
+        while(i>=0 && s.charAt(i)=='1')
+        {
+           
+                i-=1;
+            
+        }
+        return s.substring(0,i+1);
+        
+    }
+}

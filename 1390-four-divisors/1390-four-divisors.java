135ae2e7b30e5122d52182973d6c0a69e0dd894c
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-01-04 17:03:34 +0530

    Time: 72 ms (13.38%), Space: 47.2 MB (14.65%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int fin=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            HashSet<Integer> set=new HashSet<>();
+            int c=0;
+            for(int j=1;j<=Math.sqrt(nums[i]);j++)
+            {
+                int s1=nums[i]%j;
+                if(s1==0)
+                {
+                    c+=j;
+                    set.add(j);
+                    int s2=nums[i]/j;
+                    if(!set.contains(s2))
+                    {
+                        c+=s2;
+                        set.add(s2);
+                    }
+                }
+
+            }
+            if(set.size()==4)
+            {
+                fin+=c;
+            }
+        }
+        return fin;
+    }
+}

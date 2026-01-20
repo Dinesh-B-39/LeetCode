002d56ca550e8e5320a2297ef74dd879d7996c41
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-01-21 00:39:43 +0530

    Time: 4 ms (51.79%), Space: 46.6 MB (11.26%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,41 +1,39 @@
 class Solution {
-    public int check(List<List<Integer>> list, ArrayList<ArrayList<Integer>> dp,int i,int j)
+    public int check(List<List<Integer>> triangle,int i,int j,ArrayList<ArrayList<Integer>> dp)
     {
-        // if(i==0 && j==0)
-        // {
-        //     return dp.get(0).get(0);
-        // }
-        if(i<0 || j<0 || list.get(i).size()<=j)
+        if(i<0 || j<0 || triangle.get(i).size()<=j)
         {
             return Integer.MAX_VALUE;
+        }
+        if(i==0 && j==0)
+        {
+            return triangle.get(i).get(j);
         }
         if(dp.get(i).get(j)!=null)
         {
             return dp.get(i).get(j);
         }
-        int s1=check(list,dp,i-1,j);
-        int s2=check(list,dp,i-1,j-1);
-        dp.get(i).set(j,Math.min(s1,s2)+list.get(i).get(j));
+        int top=check(triangle,i-1,j,dp);
+        int dig=check(triangle,i-1,j-1,dp);
+        dp.get(i).set(j,Math.min(top,dig)+triangle.get(i).get(j));
         return dp.get(i).get(j);
+
     }
     public int minimumTotal(List<List<Integer>> triangle) {
         ArrayList<ArrayList<Integer>> dp=new ArrayList<>();
         for(int i=0;i<triangle.size();i++)
         {
-            ArrayList<Integer> list=new ArrayList<>();
+            ArrayList<Integer> d1=new ArrayList<>();
             for(int j=0;j<triangle.get(i).size();j++)
             {
-                list.add(null);
+                d1.add(null);
             }
-            dp.add(list);
+            dp.add(d1);
         }
         int mine=Integer.MAX_VALUE;
-        dp.get(0).set(0,triangle.get(0).get(0));
-        // System.out.println(dp.get(0).get(0));
         for(int i=0;i<triangle.get(triangle.size()-1).size();i++)
         {
-            int u=check(triangle,dp,triangle.size()-1,i);
-            mine=Math.min(mine,u);
+            mine=Math.min(mine,check(triangle,triangle.size()-1,i,dp));
         }
         return mine;
     }

cc7241b149f986ef1f084c7bdeab2f1767a89bca
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-01-20 23:03:56 +0530

    Time: 1 ms (18.44%), Space: 43.4 MB (70.47%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,30 +1,38 @@
 class Solution {
-    public int check(int[][] grid,int[][] dp,int i,int j)
-    {
-        if(i<0 || j<0 || grid[i][j]==1)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        int top=check(grid,dp,i-1,j);
-        int left=check(grid,dp,i,j-1);
-        dp[i][j]=top+left;
-        return top+left;
-    }
     public int uniquePathsWithObstacles(int[][] grid) {
         int[][] dp=new int[grid.length][grid[0].length];
+        if(grid[0][0]!=1)
+        {
+            dp[0][0]=1;
+        }
         for(int i=0;i<grid.length;i++)
         {
             for(int j=0;j<grid[0].length;j++)
             {
-                dp[i][j]=-1;
+                if(i==0 && j==0)
+                {
+                    continue;
+                }
+                if(grid[i][j]==1)
+                {
+                    dp[i][j]=0;
+                }
+                else
+                {
+                    int top=0;
+                    int left=0;
+                    if(i>0)
+                    {
+                        top=dp[i-1][j];
+                    }
+                    if(j>0)
+                    {
+                        left=dp[i][j-1];
+                    }
+                    dp[i][j]=top+left;
+                }
             }
         }
-        dp[0][0]=1;
-        int m=check(grid,dp,grid.length-1,grid[0].length-1);
-        return m;
+        return dp[dp.length-1][dp[0].length-1];
     }
 }

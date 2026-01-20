85c09d47e5923fb47853ea7a9c24bc130fd43afe
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-01-20 22:46:33 +0530

    Time: 1 ms (14.93%), Space: 42.1 MB (72.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,35 +1,28 @@
 class Solution {
-    public int check(int[][] dp,int i,int j)
-    {
-        if(i<0 || j<0)
-        {
-            return 0;
-        }
-        // if(i==0 && j==0)
-        // {
-        //     return 1;
-        // }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        int s1=check(dp,i-1,j);
-        int s2=check(dp,i,j-1);
-        dp[i][j]=s1+s2;
-        return s1+s2;
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                dp[i][j]=-1;
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up=0;
+                int left=0;
+                if(i>0)
+                {
+                    up=dp[i-1][j];
+                }
+                if(j>0)
+                {
+                    left=dp[i][j-1];
+                }
+                dp[i][j]=up+left;
             }
         }
-        dp[0][0]=1;
-        int m1=check(dp,m-1,n-1);
-        return m1;
-        
+        return dp[m-1][n-1];
     }
 }

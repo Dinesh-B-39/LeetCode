d87809b6c98c41fcd14b79a0ed2e632de6425129
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-01-26 22:12:20 +0530

    Time: 37 ms (11.44%), Space: 47.8 MB (6.88%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,55 @@
+class Solution {
+    int mine=Integer.MAX_VALUE;
+    int mod=1000000007;
+    public int check(int[] arr,int[][] dp,int target,int ind)
+    {
+        if(ind==-1)
+        {
+            return mod;
+        }
+        if(target==0)
+        {
+            return 0;
+        }
+        if(ind==0)
+        {
+            if(arr[ind]==target)
+            {
+                return 1;
+            }
+            // else
+            // {
+            //     return mod;
+            // }
+        }
+        if(dp[ind][target]!=-1)
+        {
+            return dp[ind][target];
+        }
+        int non_take=check(arr,dp,target,ind-1);
+        int take=Integer.MAX_VALUE;
+        if(arr[ind]<=target)
+        {
+            take=1+check(arr,dp,target-arr[ind],ind);
+        }
+        dp[ind][target]=Math.min(non_take,take);
+        return dp[ind][target];
+    }
+    public int coinChange(int[] coins, int amount) {
+        int [][] dp=new int[coins.length][amount+1];
+        for(int i=0;i<dp.length;i++)
+        {
+            for(int j=0;j<dp[0].length;j++)
+            {
+                dp[i][j]=-1;
+            }
+        }
+        int z=check(coins,dp,amount,coins.length-1);
+        if(z==mod)
+        {
+            return -1;
+        }
+        return z;
+        
+    }
+}

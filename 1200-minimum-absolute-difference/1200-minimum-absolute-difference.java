ac68530af4898178ce7e3f863851335ec5f30145
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-01-26 11:02:37 +0530

    Time: 23 ms (17.74%), Space: 64 MB (32.26%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.java
@@ -7,14 +7,16 @@ class Solution {
             mine=Integer.min(mine,Math.abs(arr[i]-arr[i+1]));
         }
         List<List<Integer>> res=new ArrayList<>();
+          List<Integer> list=new ArrayList<>();
+          list.add(-1);
+          list.add(-1);
         for(int i=0;i<arr.length-1;i++)
         {
             if(Math.abs(arr[i]-arr[i+1])==mine)
             {
-                List<Integer> list=new ArrayList<>();
-                list.add(arr[i]);
-                list.add(arr[i+1]);
-                res.add(list);
+                list.set(0,arr[i]);
+                list.set(1,arr[i+1]);
+                res.add(new ArrayList<>(list));
             }
         }
         return res;

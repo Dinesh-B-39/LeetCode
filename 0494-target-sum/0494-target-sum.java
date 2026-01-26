f5e1ff1f346d2897cd7e2e029283304b2b550b84
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-01-26 18:00:50 +0530

    Time: 9 ms (55.9%), Space: 46.3 MB (32.76%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int check(int[] nums,int ind,int target,int val)
+    int offset=0;
+    public int check(int[] nums,int ind,int target,int val,int[][]dp)
     {
         if(ind==0)
         {
@@ -16,12 +17,31 @@ class Solution {
                 return 0;
             }
         }
-        int plus=check(nums,ind-1,target,val+nums[ind]);
-        int minus=check(nums,ind-1,target,val-nums[ind]);
+        if(dp[ind][val+offset]!=-1)
+        {
+            return dp[ind][val+offset];
+        }
+        int plus=check(nums,ind-1,target,val+nums[ind],dp);
+        int minus=check(nums,ind-1,target,val-nums[ind],dp);
+        dp[ind][val+offset]=plus+minus;
         return plus+minus;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return check(nums,nums.length-1,target,0);
+        int c=0;
+        for(int i:nums)
+        {
+            c+=i;
+        }
+        int[][] dp=new int[nums.length][(c*2)+1];
+        for(int i=0;i<dp.length;i++)
+        {
+            for(int j=0;j<dp[0].length;j++)
+            {
+                dp[i][j]=-1;
+            }
+        }
+        offset=c;
+        return check(nums,nums.length-1,target,0,dp);
         
     }
 }

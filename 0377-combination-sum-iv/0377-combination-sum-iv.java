1bfeecdda92298757b390c73319a7148888b8820
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-01-31 11:32:01 +0530

    Time: 0 ms (100%), Space: 42.8 MB (55.74%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,33 @@
+class Solution {
+    // int res=0;
+    public int check(int ind,int[] nums,int target,int[] dp)
+    {
+        // if(target==0)
+        // {
+        //     res+=1;
+        //     return;
+        // }
+        if(dp[target]!=-1)
+        {
+            return dp[target];
+        }
+        int zoo=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]<=target)
+            {
+                zoo+=check(i,nums,target-nums[i],dp);
+            }
+        }
+        dp[target]=zoo;
+        return zoo;
+    }
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp=new int[target+1];
+        Arrays.fill(dp,-1);
+        dp[0]=1;
+        return check(0,nums,target,dp);
+        
+        
+    }
+}

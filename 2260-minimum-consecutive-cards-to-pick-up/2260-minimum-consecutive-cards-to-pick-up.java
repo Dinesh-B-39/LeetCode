1003e020397a4f5d2434298c34579e11a1c83daf
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-02-09 20:39:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minimumCardPickup(int[] cards) {
+        int mine=Integer.MAX_VALUE;
+        int i=0;
+        int j=0;
+        HashSet<Integer> set=new HashSet<>();
+        while(i<cards.length && j<cards.length)
+        {
+            if(set.contains(cards[j]))
+            {
+                System.out.println(cards[j]);
+                while(cards[i]!=cards[j])
+                {
+                    set.remove(cards[i]); 
+                    i+=1;
+                }
+                mine=Math.min(mine,j-i+1);
+                // set.add(cards[j]);
+                j+=1;
+            }
+
+            else
+            {
+                set.add(cards[j]);
+                j+=1;
+            }
+        }
+        if(mine==Integer.MAX_VALUE)
+        {
+            return -1;
+        }
+        return mine;
+    }
+}

9c422e04c36b619084b5205cbf773805e7a63020
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-02-10 16:18:39 +0530

    Time: 4 ms (1.44%), Space: 43.2 MB (20.54%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -5,38 +5,55 @@ class Solution {
         int bottom=matrix.length;
         int left=0;
         int right=matrix[0].length;
+        boolean[][] temp=new boolean[matrix.length][matrix[0].length];
         while(left<right && top<bottom)
         {
             for(int i=left;i<right;i++)
             {
-                System.out.println(matrix[top][i]);
-                list.add(matrix[top][i]);
+                if(temp[top][i]!=true)
+                {
+                     System.out.println(matrix[top][i]);
+                    list.add(matrix[top][i]);
+                    temp[top][i]=true;
+                }
+               
             }
             top+=1;
            
             for(int i=top;i<bottom;i++)
             {
+                  if(temp[i][right-1]!=true)
+                {
                  System.out.println(matrix[i][right-1]);
                 list.add(matrix[i][right-1]);
+                 temp[i][right-1]=true;
+                }
             }
             right-=1;
-            if(top<bottom)
-            {
+           
                  for(int i=right-1;i>=left;i--)
             {
+                 if(temp[bottom-1][i]!=true)
+                {
                 System.out.println(matrix[bottom-1][i]);
                 list.add(matrix[bottom-1][i]);
+                temp[bottom-1][i]=true;
+                }
             }
-            }
+            
             bottom-=1;
-           if(left<right)
-           {
+           
             for(int i=bottom-1;i>=top;i--)
             {
+                
+                  if(temp[i][left]!=true)
+                {
                 System.out.println(matrix[i][left]);
                 list.add(matrix[i][left]);
+                temp[i][left]=true;
+                }
             }
-           }
+           
             left+=1;
             
         }

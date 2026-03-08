a622242608f5aa93bb7f07d7a0c5d540961aa49c
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-03-08 15:54:31 +0530

    Time: 3 ms (29.39%), Space: 43.4 MB (50.14%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    int flag=0;
+    String res="";
+    public void check(String s,int limit,HashSet<String> set)
+    {
+        if(flag==1)
+        {
+            return;
+        }
+        if(s.length()==limit)
+        {
+            if(!set.contains(s))
+            {
+                res=s;
+                flag=1;
+            }
+            return;
+        }
+        check(s+"0",limit,set);
+        check(s+"1",limit,set);
+    }
+    public String findDifferentBinaryString(String[] nums) {
+        HashSet<String> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            set.add(nums[i]);
+        }
+        check("",nums[0].length(),set);
+        return res;
+        
+    }
+}

7a66b6d4670adcb4de3740a6b2dc6bfff8a5b88a
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-03-17 23:22:36 +0530

    Time: 109 ms (21.84%), Space: 71.9 MB (6.5%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,5 +1,27 @@
 class Solution {
     public int countPrimes(int n) {
+        int[] temp=new int[n];
+        Arrays.fill(temp,1);
+        for(int i=2;i<=Math.sqrt(n);i++)
+        {
+            if(temp[i]!=0)
+            {
+                for(int j=i*i;j<n;j+=i)
+                {
+                    temp[j]=0;
+                }
+            }
+        }
+        int c=0;
+        for(int i=2;i<n;i++)
+        {
+            if(temp[i]==1)
+            {
+                 c+=1;
+            }
+           
+        }
+        return c;
         
     }
 }

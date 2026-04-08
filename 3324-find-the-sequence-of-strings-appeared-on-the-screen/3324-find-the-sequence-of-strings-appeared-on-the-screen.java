bc20aaefeac097e1848c483d025588882e45eacf
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-04-08 11:22:43 +0530

    Time: 22 ms (6.28%), Space: 60.1 MB (99.03%) - LeetHub

---
+++ 3324-find-the-sequence-of-strings-appeared-on-the-screen/3324-find-the-sequence-of-strings-appeared-on-the-screen.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> stringSequence(String target) {
+        List<String> list=new ArrayList<>();
+        StringBuilder sb=new StringBuilder();
+        StringBuilder temp=new StringBuilder();
+        for(int i=0;i<target.length();i++)
+        {
+            temp.append(target.charAt(i));
+            sb.append('a');
+            list.add(sb.toString());
+           
+            while(!temp.toString().equals(sb.toString()))
+            {
+                
+                char c=sb.charAt(sb.length()-1);
+                c=(char)((((c-'a')+1)%26)+97);
+                sb.deleteCharAt(sb.length()-1);
+                sb.append(c);
+                list.add(sb.toString());
+                
+            }
+        }
+        return list;
+    }
+}

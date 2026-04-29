47990f7d9e31c4b8803b47514d2c7e913ff28694
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-04-29 17:39:45 +0530

    Time: 40 ms (5.32%), Space: 186.3 MB (5.01%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,68 +1,94 @@
 class Solution {
-    boolean flag = false;
-
-    public void check(char[][] grid, int i, int j, int pi, int pj, char c, int[][] temp) {
-
-        // right
-        if(j+1<grid[0].length && grid[i][j+1]==c) {
-            if(temp[i][j+1]==-1 && !(i==pi && j+1==pj)) {
-                flag = true;
-                return;
+    boolean flag=false;
+    public void check(int i,int j,char[][] grid,int[][] temp,int p1,int p2)
+    {
+        //top
+        if(flag==true)
+        {
+            return;
+        }
+        if(i-1>=0 && grid[i-1][j]==grid[i][j])
+        {
+            if(temp[i-1][j]!=-1)
+            {
+                temp[i-1][j]=-1;
+                check(i-1,j,grid,temp,i,j);
             }
-            if(temp[i][j+1]!=-1) {
-                temp[i][j+1] = -1;
-                check(grid, i, j+1, i, j, c, temp);
+            else if(temp[i-1][j]==-1 && (i-1!=p1 && j!=p2))
+            {
+                flag=true;
+                System.out.println(i+" "+j);
+                return;
             }
         }
 
-        // down
-        if(i+1<grid.length && grid[i+1][j]==c) {
-            if(temp[i+1][j]==-1 && !(i+1==pi && j==pj)) {
-                flag = true;
+        //down
+        if(i+1<grid.length && grid[i+1][j]==grid[i][j])
+        {
+            if(temp[i+1][j]!=-1)
+            {
+                temp[i+1][j]=-1;
+                check(i+1,j,grid,temp,i,j);
+
+            }
+            else if(temp[i+1][j]==-1 && (i+1!=p1 && j!=p2))
+            {
+                flag=true;
+                System.out.print(i+" "+j);
                 return;
             }
-            if(temp[i+1][j]!=-1) {
-                temp[i+1][j] = -1;
-                check(grid, i+1, j, i, j, c, temp);
-            }
+
         }
 
-        // left
-        if(j-1>=0 && grid[i][j-1]==c) {
-            if(temp[i][j-1]==-1 && !(i==pi && j-1==pj)) {
-                flag = true;
-                return;
+        //left
+        if(j-1>=0 && grid[i][j-1]==grid[i][j])
+        {
+            if(temp[i][j-1]!=-1)
+            {
+                temp[i][j-1]=-1;
+                check(i,j-1,grid,temp,i,j);
             }
-            if(temp[i][j-1]!=-1) {
-                temp[i][j-1] = -1;
-                check(grid, i, j-1, i, j, c, temp);
+            else if(temp[i][j-1]==-1 && (i!=p1 && j-1!=p2))
+            {
+                flag=true;
+                System.out.print(i+" "+j);
+                return;
             }
         }
-
-        // up
-        if(i-1>=0 && grid[i-1][j]==c) {
-            if(temp[i-1][j]==-1 && !(i-1==pi && j==pj)) {
-                flag = true;
+        //right
+        if(j+1<grid[0].length && grid[i][j+1]==grid[i][j])
+        {
+            if(temp[i][j+1]!=-1)
+            {
+                temp[i][j+1]=-1;
+                check(i,j+1,grid,temp,i,j);
+            }
+            else if(temp[i][j+1]==-1 && (i!=p1 && j+1!=p2))
+            {
+                flag=true;
+                System.out.print(i+" "+j);
                 return;
             }
-            if(temp[i-1][j]!=-1) {
-                temp[i-1][j] = -1;
-                check(grid, i-1, j, i, j, c, temp);
-            }
+
         }
     }
-
     public boolean containsCycle(char[][] grid) {
-        int[][] temp = new int[grid.length][grid[0].length];
-
-        for(int i=0;i<grid.length;i++) {
-            for(int j=0;j<grid[0].length;j++) {
-                if(temp[i][j] != -1) {   // ✅ important
-                    flag = false;
-                    temp[i][j] = -1;
-                    check(grid, i, j, -1, -1, grid[i][j], temp);
-                    if(flag) return true;
+        int[][] temp=new int[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                flag=false;
+                if(temp[i][j]!=-1)
+                {
+                    temp[i][j]=-1;
+                    check(i,j,grid,temp,-1,-1);
+                    if(flag==true)
+                    {
+                        return true;
+                    }
                 }
+                
             }
         }
         return false;

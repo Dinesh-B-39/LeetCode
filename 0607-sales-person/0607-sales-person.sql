06cb7ffd5a8ef98b4095981b3c2a8b307a2b8841
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-05-20 02:36:35 +0530

    Time: 1532 ms (27.16%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select name from salesperson where name not  in (
+select distinct s.name from salesperson s left join orders o
+                            on s.sales_id=o.sales_id
+                            left join company c
+                            on o.com_id=c.com_id
+                            where c.name like "RED" 
+                           )

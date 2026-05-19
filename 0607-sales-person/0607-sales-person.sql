0a5f0e6cf193a687840f0ea9bcdff1dfe2013b2d
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-05-20 02:36:57 +0530

    Time: 1446 ms (43.7%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select name from salesperson where name not  in (
-select distinct s.name from salesperson s left join orders o
+select s.name from salesperson s left join orders o
                             on s.sales_id=o.sales_id
                             left join company c
                             on o.com_id=c.com_id

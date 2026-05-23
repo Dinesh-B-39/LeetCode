8adb70abd2e6c95433d61d666c1815db1b81f5b1
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-05-23 22:33:38 +0530

    Time: 0 ms (100%), Space: 43.2 MB (11.32%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,31 +1,42 @@
 class Solution {
-    public boolean check(int[] nums) {
-        int ind=-1;
-        int z=0;
-        for(int i=1;i<nums.length;i++)
+    public boolean check1(int[] res)
+    {
+        for(int i=1;i<res.length;i++)
         {
-            if(nums[i]<nums[i-1])
-            {
-                ind=i;
-                z+=1;
-            }
-            if(z>1)
-            {
-                return false;
-            }
-        }
-
-        if(ind==-1)
-        {
-            return true;
-        }
-        for(int i=ind;i<nums.length;i++)
-        {
-            if(nums[i]>nums[0])
+            if(res[i]<res[i-1])
             {
                 return false;
             }
         }
         return true;
+    }
+    public boolean check(int[] nums) {
+        int[] res=new int[nums.length];
+        int tar=-1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]<nums[i-1])
+            {
+                tar=i;
+                break;
+            }
+        }
+        if(tar==-1)
+        {
+            return true;
+        }
+        int j=0;
+        for(int i=tar;i<nums.length;i++)
+        {
+            res[j]=nums[i];
+            j+=1;
+        }
+        for(int i=0;i<tar;i++)
+        {
+            res[j]=nums[i];
+            j+=1;
+        }
+        return check1(res);
+      
     }
 }

16dcb5603c0a2adcd1285e2ac7c04fd7c5454ad6
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-06-05 08:54:31 +0530

    Time: 892 ms (37.86%), Space: 0B (100%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+select u.name,IFNULL(sum(distance),0) as travelled_distance from users u left join Rides r on u.id=r.user_id group by u.id order by travelled_distance desc,name asc

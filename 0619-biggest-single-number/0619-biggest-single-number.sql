30afebd45a236b4baed96c5599d3ff00ca26e5bb
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-06-06 09:21:11 +0530

    Time: 412 ms (86.34%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
-select ifnull(
-(select num from mynumbers group by num having count(num)=1 order by num desc limit 1),
-null
-
-) as num
+select max(num) as num from(
+select num from MyNumbers group by num having count(*)=1 
+) T;

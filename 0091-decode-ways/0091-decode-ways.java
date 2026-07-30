7635ac17e6c586b25948c88f4dc871bdccbb949c
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-07-31 01:12:38 +0530

    Time: 2 ms (9.96%), Space: 43.9 MB (5.51%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,49 +1,50 @@
 class Solution {
-    public int check(String s,int ind,int[] dp)
+   
+    public int check(int ind,String s,HashSet<String> set,int[] dp)
     {
-        if(ind==0 )
-        {
-            if(s.charAt(ind)=='0')
-            {
-                 return 0;
-            }
-            else
-            {
-                return 1;
-            }
-           
-        }
-        if(ind==-1)
+        if(ind>=s.length())
         {
             return 1;
         }
-        if(dp[ind]!=-1)
-        {
-            return dp[ind];
-        }
-        int take=0;
-        int non_take=0;
+      if(ind==s.length()-1)
+      {
         if(s.charAt(ind)!='0')
         {
-            take=check(s,ind-1,dp);
+            return 1;
         }
-        if(ind>=1)
+        else
         {
-     
-             int p=Integer.parseInt(s.substring(ind-1,ind+1));
-             if(s.charAt(ind-1)!='0' && p>=10 && p<=26)
-            {
-                non_take=check(s,ind-2,dp);
-            }
+            return 0;
         }
-       dp[ind]=take+non_take;
-        return take+non_take;
+      }
+      if(dp[ind]!=-1)
+      {
+        return dp[ind];
+      }
+      int take=0;
+      int not_take=0;
+      if(s.charAt(ind)!='0')
+      {
+        take=check(ind+1,s,set,dp);
+      }
+      String s1=s.substring(ind,ind+2);
+      if(set.contains(s1))
+      {
+        not_take=check(ind+2,s,set,dp);
+      }
+
+      return dp[ind]=take+not_take;
     }
     public int numDecodings(String s) {
-
+        HashSet<String> set=new HashSet<>();
+        for(int i=1;i<27;i++)
+        {
+            set.add(i+"");
+        }
         int[] dp=new int[s.length()];
-        Arrays.fill(dp,-1);   
-        return check(s,s.length()-1,dp);
+        Arrays.fill(dp,-1);
+        return check(0,s,set,dp);
+        
         
     }
 }

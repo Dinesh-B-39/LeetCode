db3fac9811602196ccc6fe638f5ebdb3a8477756
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-07-31 18:58:53 +0530

    Time: 67 ms (7.23%), Space: 48 MB (52.6%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,6 +1,5 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashSet<Character> set=new HashSet<>();
         HashMap<Character,Integer> map=new HashMap<>();
         for(int i=0;i<word.length();i++)
         {
@@ -12,12 +11,11 @@ class Solution {
         {
             arr[j1]=map.get(i);
             j1+=1;
-            System.out.print(i+":"+map.get(i));
-            System.out.println();
+           
         }
         Arrays.sort(arr);
         int res=0;
-        // int c=1;
+       
         int n=0;
         for(int i=arr.length-1;i>=0;i-=8)
         {
@@ -27,21 +25,7 @@ class Solution {
                 res+=arr[j]*n;
             }
         }
-        // for(int i=0;i<word.length();i++)
-        // {
-        //     if(n%9==0)
-        //     {
-        //         c+=1;
-        //     }
-        //     if(!set.contains(word.charAt(i)))
-        //     {
-        //         // System.out.print(word.charAt(i)+" "+map.get(word.charAt(i)));
-        //         // System.out.println();
-        //         res+=c*map.get(word.charAt(i));
-        //         set.add(word.charAt(i));
-        //         n+=1;
-        //     }
-        // }
+      
         return res;
         
     }

b87abe990ccfb436c62912450d4c2a1f0c7e1339
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-08-03 12:54:31 +0530

    Time: 18 ms (25.97%), Space: 57 MB (33.29%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,42 +1,37 @@
 class Solution {
-    
-    public int check(int target,int[] nums,int ind,int[][] dp)
+    int res=0;
+    public int check(int ind,int[] arr,int k,int[][] dp)
     {
-        if(target==0)
-        {
-            return 1;
-        }
-        if(ind==0 && nums[ind]==target)
-        {
-            return 1;
-        }
         if(ind==-1)
         {
+            if(k==0)
+            {
+                return 1;
+            }
             return 0;
         }
-        if(dp[ind][target]!=-1)
+        if(dp[ind][k]!=-1)
         {
-            return dp[ind][target];
+            return dp[ind][k];
         }
-        int non_take=check(target,nums,ind-1,dp);
-        int take=0;
-        if(nums[ind]<=target)
+        int s1=0;
+        int s2=0;
+        if(arr[ind]<=k)
         {
-            take=check(target-nums[ind],nums,ind,dp);
+            s1=check(ind,arr,k-arr[ind],dp);
         }
-        dp[ind][target]=take+non_take;
-        return take+non_take;
+        s2=check(ind-1,arr,k,dp);
+        return dp[ind][k]=s1+s2;
+
     }
     public int change(int amount, int[] coins) {
+        Arrays.sort(coins);
         int[][] dp=new int[coins.length][amount+1];
         for(int i=0;i<dp.length;i++)
         {
-            for(int j=0;j<dp[0].length;j++)
-            {
-                dp[i][j]=-1;
-            }
+            Arrays.fill(dp[i],-1);
         }
-        return check(amount,coins,coins.length-1,dp);
+        return check(coins.length-1,coins,amount,dp);
         
         
     }

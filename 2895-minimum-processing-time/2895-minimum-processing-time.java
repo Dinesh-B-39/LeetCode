fc656f93d81cec685d76792794baff530fe0ac82
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-08-28 20:17:30 +0530

    Time: 101 ms (40.99%), Space: 87.2 MB (26.71%) - LeetHub

--- 2895-minimum-processing-time/2895-minimum-processing-time.java
+++ 2895-minimum-processing-time/2895-minimum-processing-time.java
@@ -3,18 +3,12 @@ class Solution {
         Collections.sort(pT);
         Collections.sort(tasks,Collections.reverseOrder());
         int m1=0;
-        int j1=0;
-        for(int i=0;i<tasks.size();i+=4)
+        int p=0;
+        for(int i=0;i<pT.size();i++)
         {
-            int maxe=0;
-            for(int j=i;j<Math.min(i+4,tasks.size());j++)
-            {
-                maxe=Math.max(maxe,tasks.get(j));
-            }
-            m1=Math.max(m1,pT.get(j1)+maxe);
-            j1+=1;
+            m1=Math.max(m1,pT.get(i)+tasks.get(p));
+            p+=4;
         }
-
         return m1;
     }
 }

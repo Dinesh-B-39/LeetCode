e0fb3994f116fff4aaf82e09d5810a31ee217139
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-09-02 00:09:45 +0530

    Time: 0 ms (100%), Space: 44.1 MB (75.38%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,20 +1,21 @@
 class Solution {
     List<List<Integer>> res=new ArrayList<>();
-    public void check(int ind,int[] nums,ArrayList<Integer> list)
+    public void check(int ind,ArrayList<Integer> list,int[] nums)
     {
         if(ind==nums.length)
         {
-            res.add(new ArrayList<Integer>(list));
+            res.add(new ArrayList<>(list));
             return;
         }
         list.add(nums[ind]);
-        check(ind+1,nums,list);
+        check(ind+1,list,nums);
         list.remove(list.size()-1);
-        check(ind+1,nums,list);
+        check(ind+1,list,nums);
     }
+
     public List<List<Integer>> subsets(int[] nums) {
         ArrayList<Integer> list=new ArrayList<>();
-        check(0,nums,list);
+        check(0,list,nums);
         return res;
         
     }

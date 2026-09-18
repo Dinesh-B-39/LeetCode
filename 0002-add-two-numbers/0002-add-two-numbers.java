5bdbf59083156d52caa80871c1cd7e3c183148ae
Dinesh-B-39 <231001039@rajalakshmi.edu.in>
2026-09-19 01:13:42 +0530

    Time: 1 ms (100%), Space: 46.4 MB (76.97%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -9,55 +9,64 @@
  * }
  */
 class Solution {
+    public ListNode reverse(ListNode head)
+    {
+        ListNode prev=null;
+        while(head!=null)
+        {
+            ListNode temp=head.next;
+            head.next=prev;
+            prev=head;
+            head=temp;
+        }
+        return prev;
+    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // ListNode h1=reverse(l1);
         ListNode dummy=new ListNode(-1);
         ListNode temp=dummy;
-        
+        // ListNode h2=reverse(l2);
+        int carry=0;
         ListNode temp1=l1;
         ListNode temp2=l2;
-
-        int carry=0;
         while(temp1!=null && temp2!=null)
         {
-            int c1=temp1.val+temp2.val+carry;
-            carry=c1/10;
-            ListNode doop=new ListNode(c1%10);
-            temp.next=doop;
+            int val=temp1.val+temp2.val+carry;
+            temp1.val=val%10;
+            carry=val/10;
+            ListNode tempo=temp1.next;
+            temp.next=temp1;
             temp=temp.next;
-
-            temp1=temp1.next;
+            temp.next=null;
+            temp1=tempo;
             temp2=temp2.next;
         }
-        while(temp1!=null)
-        {
-            int c2=temp1.val+carry;
-            carry=c2/10;
-            ListNode doop2=new ListNode(c2%10);
-            temp.next=doop2;
+       while(temp1!=null)
+       {
+            int val=temp1.val+carry;
+            temp1.val=val%10;
+            carry=val/10;
+            ListNode tempo=temp1.next;
+            temp.next=temp1;
             temp=temp.next;
-
-            temp1=temp1.next;
-
-        }
-        while(temp2!=null)
-        {
-            int c2=temp2.val+carry;
-            carry=c2/10;
-            ListNode doop2=new ListNode(c2%10);
-            temp.next=doop2;
+            temp.next=null;
+            temp1=tempo;
+       }
+       while(temp2!=null)
+       {
+            int val=temp2.val+carry;
+            temp2.val=val%10;
+            carry=val/10;
+            ListNode tempo=temp2.next;
+            temp.next=temp2;
             temp=temp.next;
-
-            temp2=temp2.next; 
-        }
-        while(carry!=0)
-        {
-          ListNode doop4=new ListNode(carry%10);
-            temp.next=doop4;
-            temp=temp.next;
-
-            carry=carry/10;
-
-        }
+            temp.next=null;
+            temp2=tempo;
+       }
+       if(carry!=0)
+       {
+        temp.next=new ListNode(carry);
+       }
         return dummy.next;
     }
 }
